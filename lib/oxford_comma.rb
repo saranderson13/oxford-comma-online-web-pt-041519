5be4ade3774f2c4a1dce4dd25bf1707ca8380087
Sarah Anderson <saranderson13@gmail.com>
2019-04-21 23:12:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     when array.length == 1
       array[0]
     when array.length == 2
-      array[0] and array[1]
+      "#{array[0]} and #{array[1]}"
     when array.length > 2
       
     end
