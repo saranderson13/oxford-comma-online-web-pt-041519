b7afe3b8a240683aece0e30030218a01a76dd697
Sarah Anderson <saranderson13@gmail.com>
2019-04-21 23:20:10 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  case 
+    when array.length == 1
+      array[0]
+    when array.length == 2
+      "#{array[0]} and #{array[1]}"
+    when array.length > 2
+      last_element = array.last
+      array.pop()
+      string = array.join(", ")
+      string << ", and #{last_element}"
+    end
 end
