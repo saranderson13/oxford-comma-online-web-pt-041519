b9df9a11d99594417d3b6119550d2932530d6244
Sarah Anderson <saranderson13@gmail.com>
2019-04-21 23:11:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  case |array|
+    when array.length = 1
+      array[0]
+    when array.length = 2
+      array[0] and array[1]
+    when array.length > 2
+      
+    end
 end
