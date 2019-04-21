13e3f74a62277034e48d3fb385c68dacd45f3790
Sarah Anderson <saranderson13@gmail.com>
2019-04-21 23:18:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,6 +8,8 @@ def oxford_comma(array)
       "#{array[0]} and #{array[1]}"
     when array.length > 2
       binding.pry
-      array.join(", ")
+      last_element = array.last
+      array.pop()
+      string = array.join(", ")
     end
 end
