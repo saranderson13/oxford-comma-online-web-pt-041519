74742fd24fe9d6c718836ea1618f3eddb41be0aa
Sarah Anderson <saranderson13@gmail.com>
2019-04-21 23:11:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   case |array|
-    when array.length = 1
+    when array.length == 1
       array[0]
-    when array.length = 2
+    when array.length == 2
       array[0] and array[1]
     when array.length > 2
       
